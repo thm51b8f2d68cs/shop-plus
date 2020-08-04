9e100b2197f6be4f7c3f1616c84f38cc7323f031
thm51b8f2d68cs <tyypoh@outlook.com>
2020-08-04 13:33:02 -0700

    Adjusted position of title.

--- drawFrontPage.lua
+++ drawFrontPage.lua
@@ -20,7 +20,7 @@ function main()
     monitor.setPaletteColor(colors.black, 0x050505)
     --Draw title
     drawBackground(15, 3, 18, 24, colors.black)
-    drawTitle(5, 1)
+    drawTitle(6, 1)
 
 
     --Set colors for categories background
