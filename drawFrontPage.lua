46c74e767531695f8304d3cf2c9445ac8f1d5f89
thm51b8f2d68cs <tyypoh@outlook.com>
2020-08-04 20:13:48 -0700

    Uncommented drawing categories.

--- drawFrontPage.lua
+++ drawFrontPage.lua
@@ -29,7 +29,7 @@ function main()
     drawBackground(1, 1, 2, 6, colors.white)
 
     --Draw categories
-    --drawCategories(2, 2)
+    drawCategories(2, 2)
 
     --Set text size for report bug
     --Draw report bug
