b200193f567b0030e28e5234ad6de436ccbd9944
thm51b8f2d68cs <tyypoh@outlook.com>
2020-08-04 22:50:12 -0700

    Background resizing; repositioning.

--- drawFrontPage.lua
+++ drawFrontPage.lua
@@ -23,8 +23,8 @@ function main()
     drawBackground(1, 1, 150, 62, colors.black)
 
     --Draw title
-    drawBackground(20, 2, 112, 10, colors.gray)
-    drawTitle(20, 2)
+    drawBackground(21, 2, 112, 10, colors.gray)
+    drawTitle(21, 2)
 
     --Draw categories background
 
