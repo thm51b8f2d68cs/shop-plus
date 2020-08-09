9b034846d14ffef0927ff7ce664cc924c39b5b00
thm51b8f2d68cs <tyypoh@outlook.com>
2020-08-08 23:43:12 -0700

    Changed parseItems()

--- inventoryManager.lua
+++ inventoryManager.lua
@@ -8,14 +8,15 @@
 
 
 local function parseItems(inventory)
-local items = io.open("items.txt", "w")
-local itemData = {}
-for slot, item in pairs(inventory.list()) do
-items:write(itemData.slot = item)
-end
-items:close()
+    local items = io.open("items.txt", "w")
+    local itemData = {}
+    for slot, item in pairs(inventory.list()) do
+        itemData.slot = item
+    end
+    items:write()
+    items:close()
 end
 
 return {
-parseItems = parseItems
+    parseItems = parseItems
 }
