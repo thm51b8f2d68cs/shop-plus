2bd60e705f9a2cf9b43d22112eda063bd50b2fe5
thm51b8f2d68cs <tyypoh@outlook.com>
2020-08-08 22:43:15 -0700

    Changed parseItems() function.

--- inventoryManager.lua
+++ inventoryManager.lua
@@ -1,20 +1,20 @@
-function parseItems(inventory)
-    local items = io.open("items.txt", "w")
-    for item in inventory.list() do
-        items:write(textutils.serialize(item))
-    end
-    items:close()
+--function parseItems(inventory)
+--    local items = io.open("items.txt", "w")
+--    for item in inventory.list() do
+--        items:write(textutils.serialize(item))
+--    end
+--    items:close()
+--end
+
+
+local function parseItems(inventory)
+local items = io.open("items.txt", "w")
+for item in inventory.list() do
+items:write(textutils.serialize(item))
+end
+items:close()
 end
 
-
---local function parseItems()
---local items = io.open("items.txt", "w")
---for item in inventory.list() do
---items:write(textutils.serialize(item))
---end
---items:close()
---end
---
---return {
---parseItems = parseItems
---}
+return {
+parseItems = parseItems
+}
