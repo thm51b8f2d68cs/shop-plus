9b390c3b7043770030f559b33ab4fcb96fbf2b23
thm51b8f2d68cs <tyypoh@outlook.com>
2020-08-08 22:35:19 -0700

    Changed how api is required.

--- inventoryManager.lua
+++ inventoryManager.lua
@@ -8,29 +8,4 @@ function parseItems()
     items:close()
 end
 
--- function formatItems()
---     local items = io.open("items.txt", "rw")
---     local data = items.read("*a")
---     local catalog = textutils.unserialize(data)
---     for item in catalog do
---         item.purchases = "0"
---     end
-    
---     items:write(textutils.serialize(catalog))
---     items:close()
-
--- end
-
-function logPurchase(itemBought)
-    local items = io.open("items.txt", "r")
-    local data = items.read("*a")
-    local catalog = textutils.unserialize(data)
-    for item in catalog do
-        if (item.name == itemBought.name) then
-            item.purchases = item.purchases + 1
-        end
-    end
-
-    items:write(textutils.serialize(catalog))
-    items:close()
-end
+return
