df3b9481c70b0aa9e35b372f9c831b16c1fab286
thm51b8f2d68cs <tyypoh@outlook.com>
2020-08-08 23:13:41 -0700

    - Modulated pages. - Moved report a bug. - Adjusted size of item tile.

--- shop.lua
+++ shop.lua
@@ -138,7 +138,7 @@ function drawReportABug(x, y)
     drawBackground(x, y, 31, 51, colors.lightGray)
     monitor.setTextColor(colors.white)
     monitor.setBackgroundColor(colors.lightGray)
-    bigfont.writeOn(monitor, 1, "Report a bug", 31 / 2, y + 1)
+    bigfont.writeOn(monitor, 1, "Report a bug", x + 1, y + 1)
 end
 
 function drawMainPage(x, y, page)
