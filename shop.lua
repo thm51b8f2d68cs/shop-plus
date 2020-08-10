2160561f90faa6b7b59efef54282b4579ce33743
thm51b8f2d68cs <tyypoh@outlook.com>
2020-08-09 20:06:26 -0700

    - Changed size and spacing of tiles. - Changed size of item text.

--- shop.lua
+++ shop.lua
@@ -144,7 +144,7 @@ function drawBrowsePage()
         monitor.setBackgroundColor(colors.white)
         monitor.setCursorPos(x, y)
         local itemData = inventory.getItem(slot).getMetadata()
-        bigfont.writeOn(monitor, 1, itemData.displayName, ((x + width) / 2) + x, y)
+        bigfont.writeOn(monitor, 1, itemData.displayName, (width / 2) + x, y)
 --        monitor.write(itemData.displayName)
         if (column % 2 == 0) then
             y = y + 17
